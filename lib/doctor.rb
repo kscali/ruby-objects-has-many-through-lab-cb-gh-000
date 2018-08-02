c263078676e63aa1a83fa902dcde752745d4ddd5
Koy Saeteurn <kosatun@aol.com>
2018-08-02 23:37:46 +0000

    automatically backed up by learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -7,6 +7,20 @@ class Doctor
     @name = name 
     @@doctors << self 
   end   
-
+  
+  def new_appointment(date, patient)
+    Appointment.new(date, self, patient)
+  end   
+  
+  def appointments 
+    Appointments.all.select do |appointment|
+      appointment.doctor == self 
+    end 
+  end   
+  
+  def patients 
+    patients.map do |patient|
+      patient.appointment
+  end 
   
 end   
